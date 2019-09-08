535e4eb9dda219b33911dbed4d2b8889c6c03d84
Vishnu <VishnuUnnikrishnan@users.noreply.github.com>
2019-09-08 18:42:42 +1000

    First Release of bizQR Application!

---
+++ js/bizy.js
@@ -0,0 +1,65 @@
+$( document ).ready(function() {
+    setDate();
+    $(".qr_code").empty();
+    data = getSavedData();
+
+    if (data != null){
+        loadsavedCard(data);
+    }
+    
+});
+
+function loadsavedCard(card){
+    $(".qr_code").qrcode({
+        text:card[0]
+    });
+
+    $(".modal-header").html(card[1] + "'s Contact Details");
+    $('.qrmodal').modal('show');
+}
+
+function setDate(){
+    $("ydate").html(new Date().getFullYear());
+}
+
+function getSavedData(){
+    data = localStorage.getItem('card');
+    if(data === null){
+        return null;
+    }
+    name = localStorage.getItem('name');
+
+    return [data, name];
+}
+
+function saveData(card, name){
+    localStorage.setItem('card',card);
+    localStorage.setItem('name',name);
+}
+
+function createQR(){
+    card = "BEGIN:VCARD\nVERSION:3.0"
+    end = "\nEND:VCARD"
+    name = "\nN:"+$("#lname").val()+";"+$("#fname").val();
+    fn = "\nFN:"+$("#fname").val()+" "+$("#lname").val();
+    org = "\nORG:"+$("#company").val();
+    title = "\nTITLE:"+$("#title").val();
+    email = "\nEMAIL;WORK;INTERNET:"+$("#email").val();
+    phone = "\nTEL;CELL:"+$("#phone").val();
+    website= "\nURL:"+$("#url").val();
+    street = $("#address").val();
+    suburb = $("#suburb").val();
+    postcode = $("#postcode").val();
+    state = $("#state").val();
+    country = $("#country").val();
+    address = "\nADR:;;"+street+";"+suburb+";"+state+";"+postcode+";"+country;
+    
+    $(".modal-header").html($("#fname").val() + "'s Contact Details");
+
+    card = card+name+fn+org+title+address+phone+email+website+end;
+    $(".qr_code").empty();
+    $(".qr_code").qrcode({
+        text:card
+    });
+    saveData(card, $("#fname").val());
+}
